879e214a151ffc34a669e058097079e069401f49
ranaheta <ranaheta308@gmail.com>
2025-11-14 11:07:31 +0530

    updatehome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./home.css";
-import Contact from "./Contact";
 
+function Home() {
+  const navigate = useNavigate();
 
- function Home() {
-  const[page,setPage]=useState("Home");
-
-  const renderPage = () => {
-    if (page === "Contact") {
-      return <Contact />;
-    }
   return (
     <section className="home">
       <h2>Welcome to KarmaFinancial</h2>
@@ -17,14 +12,10 @@ import Contact from "./Contact";
         We help you grow your wealth through smart financial planning,
         investment advisory, insurance coverage, and mutual funds.
       </p>
-      
-      <button onClick={() => setPage("Contact")}>🚀 Get in Touch</button>
-      
-      
+
+      <button onClick={() => navigate("/contact")}>🚀 Get in Touch</button>
     </section>
-   
   );
 }
-return renderPage();
-}
+
 export default Home;
